46b42442f229e06440c026701c41c560e177e216
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-09-03 13:04:27 -0700

    clean up on exception

--- unpack.py
+++ unpack.py
@@ -1,19 +1,22 @@
 import subprocess as sp
 from os.path import abspath
 
+import clean_up
+
 def dmg2img(file, convertedFile=None):
     try:
         sp.call(['dmg2img', file, convertedFile])
     except BaseException:
         print('[ERROR] unable to convert file: %s\nPlease make sure that the dmg2img package is installed, and the file exists.' % abspath(file))
+        clean_up.main()
         exit(1)
 
 def unpackXar(file):
     try:
         sp.call(['xar', '-xf', file])
     except BaseException:
-        print(
-            '[ERROR] Unable to extract file: %s\nPlease make sure that the file exists, and install xar from http://bit.ly/archive-xar.' % abspath(file))
+        print('[ERROR] Unable to extract file: %s\nPlease make sure that the file exists, and install xar from http://bit.ly/archive-xar.' % abspath(file))
+        clean_up.main()
         exit(1)
 
 def unpack7z(file, outputDir = '.'):
@@ -21,5 +24,6 @@ def unpack7z(file, outputDir = '.'):
         sp.call(['7z', 'x', file, '-y', '-o%s' % outputDir])
     except BaseException:
         print('[ERROR] unable to extract file: %s\nPlease install the p7zip-full package.' % abspath(file))
+        clean_up.main()
         exit(1)
 
