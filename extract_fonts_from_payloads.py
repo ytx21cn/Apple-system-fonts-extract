46c3445afd6ef881ae3b3c5a1d1ba0c3f40dcc18
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2020-01-08 17:47:23 -0800

    use shutil to copy

--- extract_fonts_from_payloads.py
+++ extract_fonts_from_payloads.py
@@ -1,15 +1,21 @@
-import glob, subprocess as sp
+import glob
+import subprocess as sp
 from os.path import basename, dirname, splitext, abspath
+from shutil import copyfile
 
 from paths import safe_mkdir, safe_remove, pkgExtractedPath, fontPath
 from unpack import unpack_7z
 
-def unpackPayload(payloadFile, outputDir='.'):
-    copyTarget = '%s.7z.gz' % payloadFile
-    sp.call(['cp', payloadFile, copyTarget])
-    sp.call(['gunzip', copyTarget])
-    unpack_7z('%s.7z' % payloadFile, outputDir)
-    safe_remove('%s.7z' % payloadFile)
+
+def unpack_payload(payload_file, output_dir='.'):
+    source = '%s.7z' % payload_file
+    copy_target = '%s.gz' % source
+
+    copyfile(src=payload_file, dst=copy_target)
+    sp.call(['gunzip', copy_target])
+    unpack_7z(source, output_dir)
+    safe_remove(source)
+
 
 def main():
     payloadFiles = glob.glob('%s/**/Payload' % pkgExtractedPath, recursive=True)
@@ -23,7 +29,7 @@ def main():
         payloadDir = abspath(dirname(payloadFile))
         tempPath = '%s/temp' % payloadDir
 
-        unpackPayload(payloadFile, tempPath)
+        unpack_payload(payloadFile, tempPath)
         fontTargetPath = '%s/%s' % (fontPath, fontName)
         safe_mkdir(fontTargetPath)
 
