6fec79f12b9955e1b563180e1b1fd167b8dd9ebd
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2020-01-08 18:12:46 -0800

    revision and add documentation

--- dmg2img.py
+++ dmg2img.py
@@ -1,20 +1,22 @@
 import glob
-
-from os.path import basename, splitext, abspath
+from os.path import basename, splitext
 
 from paths import safe_mkdir, source_files_path, converted_files_path
 from unpack import dmg2img
 
 
 def main():
+    """
+    Convert dmg files to img files
+    """
 
     safe_mkdir(converted_files_path)
 
-    dmgFiles = glob.glob('%s/*.dmg' % source_files_path)
-    for file in dmgFiles:
-        fontName = splitext(basename(file))[0]
-        convertedFile = '%s/%s.img' % (converted_files_path, fontName)
-        dmg2img(file, convertedFile)
+    dmg_files = glob.glob('%s/*.dmg' % source_files_path)
+    for file in dmg_files:
+        font_name = splitext(basename(file))[0]
+        converted_file = '%s/%s.img' % (converted_files_path, font_name)
+        dmg2img(file, converted_file)
 
 
 if __name__ == '__main__':
