ee99848f969e3fc96665fd634e9fa8922fcf805c
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2020-01-08 18:05:41 -0800

    font extract revision

--- extract_fonts_from_payloads.py
+++ extract_fonts_from_payloads.py
@@ -1,7 +1,8 @@
 import glob
 import subprocess as sp
-from os.path import basename, dirname, splitext, abspath
+from os.path import basename, dirname, splitext
 from shutil import copyfile
+from tempfile import TemporaryDirectory
 
 from paths import safe_mkdir, safe_remove, pkgExtractedPath, fontPath
 from unpack import unpack_7z
@@ -12,31 +13,28 @@ def unpack_payload(payload_file, output_dir='.'):
     copy_target = '%s.gz' % source
 
     copyfile(src=payload_file, dst=copy_target)
-    sp.call(['gunzip', copy_target])
+    sp.call(['gunzip', '-f', copy_target])
     unpack_7z(source, output_dir)
     safe_remove(source)
 
 
 def main():
-    payloadFiles = glob.glob('%s/**/Payload' % pkgExtractedPath, recursive=True)
+    payload_files = glob.glob('%s/**/Payload' % pkgExtractedPath, recursive=True)
 
     safe_mkdir(fontPath)
 
-    for payloadFile in payloadFiles:
-        fontName = splitext(basename(dirname(payloadFile)))[0]
-        print('\n[Now copying font] %s' % fontName)
+    for payload_file in payload_files:
+        font_name = splitext(basename(dirname(payload_file)))[0]
+        font_target_path = '%s/%s' % (fontPath, font_name)
+        safe_mkdir(font_target_path)
 
-        payloadDir = abspath(dirname(payloadFile))
-        tempPath = '%s/temp' % payloadDir
+        print('\n[Now extracting font "%s"]' % font_name)
 
-        unpack_payload(payloadFile, tempPath)
-        fontTargetPath = '%s/%s' % (fontPath, fontName)
-        safe_mkdir(fontTargetPath)
-
-        otfFilesPath = '%s/**/*.otf' % tempPath
-        otfFiles = glob.glob(otfFilesPath, recursive=True)
-        for otfFile in otfFiles:
-            sp.call(['cp', '-r', otfFile, fontTargetPath])
+        with TemporaryDirectory() as temp_path:
+            unpack_payload(payload_file, temp_path)
+            otf_files = glob.glob('%s/**/*.otf' % temp_path, recursive=True)
+            for otf_file in otf_files:
+                copyfile(src=otf_file, dst='%s/%s.otf' % (font_target_path, basename(otf_file)))
 
 
 if __name__ == '__main__':
