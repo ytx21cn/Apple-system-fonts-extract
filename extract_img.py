2b3d3720f747389382b10175a803018c4f790677
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2020-01-08 18:42:51 -0800

    minor revision

--- extract_img.py
+++ extract_img.py
@@ -1,16 +1,17 @@
-import glob, subprocess as sp
-from os.path import abspath
+import glob
 
 from paths import safe_mkdir, converted_files_path, img_extracted_path
 from unpack import unpack_7z
+
 
 def main():
     safe_mkdir(img_extracted_path)
 
     # extract each img file, and put the extracted content in img_extracted_path
-    imgFiles = glob.glob('%s/*.img' % converted_files_path)
-    for file in imgFiles:
+    img_files = glob.glob('%s/*.img' % converted_files_path)
+    for file in img_files:
         unpack_7z(file, output_dir=img_extracted_path)
+
 
 if __name__ == '__main__':
     main()
