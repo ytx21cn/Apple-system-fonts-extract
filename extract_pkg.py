c2a7557a9ac1dd9e4e04926810ec2d304507a87b
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2020-01-08 18:23:26 -0800

    revised copy

--- extract_pkg.py
+++ extract_pkg.py
@@ -1,7 +1,7 @@
 import glob
-import subprocess as sp
 from os import chdir
 from os.path import dirname, basename, abspath
+from shutil import copyfile
 
 from paths import safe_mkdir, img_extracted_path, pkg_extracted_path
 from unpack import unpack_xar
@@ -13,17 +13,16 @@ def main():
     pkg files are in xar format
     """
 
-    base_dir = abspath(dirname(__file__))
-
     safe_mkdir(pkg_extracted_path)
 
+    base_dir = abspath(dirname(__file__))
     pkg_files = glob.glob('%s/**/*.pkg' % img_extracted_path, recursive=True)
     for file in pkg_files:
         font_name = basename(dirname(file))
         font_dir = abspath('%s/%s' % (pkg_extracted_path, font_name))
         safe_mkdir(font_dir)
 
-        sp.call(['cp', '-r', file, font_dir])
+        copyfile(src=file, dst='%s/%s' % (font_dir, basename(file)))
         file_to_extract = '%s/%s' % (font_dir, basename(file))
         chdir(font_dir)
         unpack_xar(file_to_extract)
