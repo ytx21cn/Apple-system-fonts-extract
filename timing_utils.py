a0df7ca6aee3b1cb517c5ca4485bc2c1e51b9faf
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2020-01-18 11:14:29 -0800

    timing utils docs

--- timing_utils.py
+++ timing_utils.py
@@ -5,6 +5,20 @@ from sys import stderr
 def time_func(func, *args, header: str = None,
               start_msg: str = None, end_msg: str = None,
               **kwargs):
+    """
+    Execute a function :param func with arguments
+    and print the time it has taken.
+    Header can be customized with :param header
+    Custom messages can be customized with :param start_msg and :param end_msg
+    :param func: the function to execute
+    :param args: positional arguments for :param func
+    :param header: custom header to print
+        before :param start_msg and :param end_msg
+    :param start_msg: the message to print before :param func starts
+    :param end_msg: the message to print after :param func ends
+    :param kwargs: keyword arguments for :param func
+    :return:
+    """
 
     func_name = func.__name__
     header = header or func_name
