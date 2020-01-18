fe72da5dfec1caa2e1fa6bd31de6edd9625831b9
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2020-01-18 15:08:31 -0800

    add safe create file

--- path_utils.py
+++ path_utils.py
@@ -1,10 +1,17 @@
-from os import makedirs
+from os import makedirs, renames
+from tempfile import mkstemp
 from shutil import rmtree
 
 
-def safe_mkdir(path: str):
-    path = str(path)
-    makedirs(path, exist_ok=True)
+def safe_mkdir(dir_path: str):
+    dir_path = str(dir_path)
+    makedirs(dir_path, exist_ok=True)
+
+
+def safe_create_file(file_path: str):
+    file_path = str(file_path)
+    temp_file_name = mkstemp(dir='.')[1]
+    renames(temp_file_name, file_path)
 
 
 def safe_remove(path: str):
