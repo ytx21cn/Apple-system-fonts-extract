d4cb4b86a926f22539c1cfd85529dee0febd9453
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2020-01-19 11:52:28 -0800

    revised timing outputs

--- timing_utils.py
+++ timing_utils.py
@@ -23,17 +23,15 @@ def time_func(func, *args, header: str = None,
     func_name = func.__name__
     header = header or func_name
 
-    print('\n[%s]' % header, file=stderr)
-    print('%s' % (start_msg or ('Starting "%s"...' % func_name)),
-          file=stderr)
+    print('\n[%s]' % header, start_msg or ('Starting "%s"...' % func_name),
+          sep='\n', file=stderr)
 
     start_time = time()
     result = func(*args, **kwargs)
     time_elapsed = time() - start_time
 
-    print('\n[%s]' % header, file=stderr)
-    print('%s' % (end_msg or ('"%s" completed' % func_name)),
-          file=stderr)
-    print('Time elapsed: %.3f seconds' % time_elapsed, file=stderr)
+    print('\n[%s]' % header, end_msg or ('"%s" completed' % func_name),
+          'Time elapsed: %.3f seconds' % time_elapsed,
+          sep='\n', file=stderr)
 
     return result
