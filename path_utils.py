67614e2034125d05c004e0c93deed500a2271156
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2020-01-18 18:01:04 -0800

    added docs

--- path_utils.py
+++ path_utils.py
@@ -1,19 +1,42 @@
-from os import makedirs, renames
-from tempfile import mkstemp
+from os import makedirs
+from os.path import dirname, abspath, isfile, isdir
 from shutil import rmtree
 
 
 def safe_mkdir(dir_path: str):
-    dir_path = str(dir_path)
-    makedirs(dir_path, exist_ok=True)
+    """
+    Safely create a directory. Supports multi-level directory creation.
+    :param dir_path: the path to create a directory.
+    """
+    dir_path = abspath(str(dir_path))
+    if isfile(dir_path):
+        pass
+    else:
+        makedirs(dir_path, exist_ok=True)
 
 
-def safe_create_file(file_path: str):
-    file_path = str(file_path)
-    temp_file_name = mkstemp(dir='.')[1]
-    renames(temp_file_name, file_path)
+def safe_create_file(file_path: str, overwrite: bool = False):
+    """
+    Safely create a file. Supports multi-level directory creation.
+    :param file_path: the path to create a directory.
+    :param overwrite: whether to overwrite if a file of that name exists
+        (do nothing if a directory of that name exists)
+    """
+    file_path = abspath(str(file_path))
+    if isdir(file_path):
+        pass
+    elif isfile(file_path) and (not overwrite):
+        pass
+    else:
+        safe_mkdir(dirname(file_path))
+        file = open(file_path, 'w')
+        file.close()
 
 
 def safe_remove(path: str):
-    path = str(path)
+    """
+    Safely remove a file / directory tree.
+    :param path: the path to remove.
+    """
+    path = abspath(str(path))
     rmtree(path, ignore_errors=True)
