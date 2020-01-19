84f64fd7fa6364fe423c1c28ba075f86ea31da15
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2020-01-18 16:07:33 -0800

    return absolute paths only at the end

--- unpack_utils.py
+++ unpack_utils.py
@@ -5,44 +5,37 @@ from os.path import basename, splitext, dirname, abspath, isdir, join
 
 # file extraction
 
-def dmg2img(dmg_file: str, target: str = None):
+def dmg2img(dmg_file: str, output: str = None):
     """
     Convert dmg to img.
     Note: This function uses the "dmg2img" command in the "dmg2img" package.
     Make sure that you have "dmg2img" package installed.
 
     :param dmg_file: the .dmg file to be converted
-    :param target: the output path / output .img file
-    :return: the actual path of the target file
+    :param output: the output path / output .img file
+    :return: the absolute path of the output file
     """
 
-    header = '\n[DMG to IMG]'
-
     # set proper paths
-    dmg_file = abspath(str(dmg_file))
+    dmg_file = str(dmg_file)
     img_filename = '%s.img' % splitext(basename(dmg_file))[0]
-    if target:
-        target = abspath(str(target))
-        if isdir(target):
-            target = join(target, img_filename)
+    if output:
+        output = str(output)
+        if isdir(output):
+            output = join(output, img_filename)
     else:
-        target = abspath(join(dirname(dmg_file), img_filename))
+        output = join(dirname(dmg_file), img_filename)
 
-    # convert .dmg to .img
-    print(header, file=stderr)
-    print('Converting "%s" to "%s"...' % (dmg_file, target), file=stderr)
     try:
-        sp.call(['dmg2img', dmg_file, target])
-        print(header, file=stderr)
-        print('Converted "%s" to "%s"' % (dmg_file, target), file=stderr)
+        sp.call(['dmg2img', dmg_file, output])
     except OSError:
-        print('[ERROR] unable to convert dmg file: %s\n'
+        print('[ERROR] unable to convert dmg file: "%s"\n'
               'Please make sure that the dmg2img package is installed,'
               'and that the .dmg file does exist.' % abspath(dmg_file),
               file=stderr)
         exit(1)
 
-    return target
+    return abspath(output)
 
 
 def unpack_7z(archive: str, output_dir: str = None):
@@ -53,30 +46,24 @@ def unpack_7z(archive: str, output_dir: str = None):
 
     :param archive: the path to the archive to be extracted
     :param output_dir: the directory to output the extracted content
-    :return: the actual output directory
+    :return: the absolute path of the actual output directory
     """
 
-    header = '\n[Unpack 7z archive]'
-
     # set proper paths
-    archive = abspath(str(archive))
+    archive = str(archive)
     if output_dir:
-        output_dir = abspath(str(output_dir))
+        output_dir = str(output_dir)
     else:
-        output_dir = abspath(dirname(archive))
+        output_dir = dirname(archive)
 
     # unpack archive
-    print(header, file=stderr)
-    print('Unpacking "%s" to "%s"...' % (archive, output_dir), file=stderr)
     try:
         sp.call(['7z', 'x', archive, '-y', '-o%s' % output_dir])
-        print(header, file=stderr)
-        print('Unpacked "%s" to "%s"' % (archive, output_dir), file=stderr)
     except OSError:
-        print('[ERROR] unable to extract file: %s\n'
+        print('[ERROR] unable to extract file: "%s"\n'
               'Please ensure that the p7zip-full package is installed,'
               'and that the file to extract does exist.' % abspath(archive),
               file=stderr)
         exit(1)
 
-    return output_dir
+    return abspath(output_dir)
