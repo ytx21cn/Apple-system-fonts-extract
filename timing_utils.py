5e7f2db1d806e8caf89f57049ca27aab184dd254
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2020-02-02 00:30:48 -0800

    revised timing output

--- timing_utils.py
+++ timing_utils.py
@@ -2,8 +2,7 @@ from time import time
 from sys import stderr
 
 
-def time_func(func, header: str = None,
-              start_msg: str = None, end_msg: str = None):
+def time_func(func):
     """
     NOTE: This is a decorator function.
 
@@ -12,29 +11,23 @@ def time_func(func, header: str = None,
     Header can be customized with :param header
     Custom messages can be customized with :param start_msg and :param end_msg
     :param func: the function to execute
-    :param header: custom header to print
-        before :param start_msg and :param end_msg
-    :param start_msg: the message to print before :param func starts
-    :param end_msg: the message to print after :param func ends
     :return: the execution result of :param func
     """
 
     func_name = func.__name__
-    header = header or func_name
 
     def inner_func(*args, **kwargs):
 
-        print('\n[%s]' % header,
-              start_msg or ('Starting "%s"...' % func_name),
+        print('\n[Starting "%s"...]' % func_name,
+              'Timing starts.',
               sep='\n', file=stderr)
 
         start_time = time()
         result = func(*args, **kwargs)
         time_elapsed = time() - start_time
 
-        print('\n[%s]' % header,
-              end_msg or ('"%s" completed' % func_name),
-              'Time elapsed: %.3f seconds' % time_elapsed,
+        print('\n["%s" completed]' % func_name,
+              'Time elapsed: %.3f seconds.' % time_elapsed,
               sep='\n', file=stderr)
         return result
 
